bddb4e2f206ffa4ec5e2973a8d41397f7b371ee0
Saksham <infoesportsclash@gmail.com>
2026-01-24 14:01:12 +0530

    update something

--- app/[...nextauth]/route.ts
+++ app/[...nextauth]/route.ts
@@ -7,14 +7,15 @@ const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
   },
+  debug: true,
 });
 
 export { handler as GET, handler as POST };
